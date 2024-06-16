f543377c4656ee173d75cb9526e91c775c7ab3a4
nimarhmti <nimarahmati2000@gmail.com>
2024-06-16 09:49:57 +0330

    api was created

--- app/(homePage)/(routes)/page.tsx
+++ app/(homePage)/(routes)/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import { PlayList } from "../_components/playList/playList";
 import { Music } from "../_components/music/Music";
 import { songsModel } from "@/types/dataType";
-export default function Home() {
+
+export default async function Home() {
   return (
     <main className="grid grid-rows-3 md:grid-rows-1 grid-cols-1 md:grid-cols-3 w-full h-5/6 md:w-3/4 bg-secondary absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 shadow-xl p-8">
       {/* current music section */}
